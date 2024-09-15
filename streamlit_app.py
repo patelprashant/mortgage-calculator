c20802895486bdc10e3e296a74bb60cd8e51ac14
Prashant <prashant.patel77@gmail.com>
2024-09-15 12:29:39 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("I am writing new code with streamlit")
+name = st.text_input("Are you enjoying Mr.?")
+st.write(f"Here we go... Mr. {name} is enjoying streamlit..whoo..hoo!!")
